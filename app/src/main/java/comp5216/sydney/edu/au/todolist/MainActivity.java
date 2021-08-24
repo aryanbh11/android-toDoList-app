6ab96b148a08b68dbec0fa8f5749428b1bc09738
Aryan Bhatia <aryanbhatia14@gmail.com>
2021-08-24 17:08:32 +1000

    Edit Item + Delet Item

--- app/src/main/java/comp5216/sydney/edu/au/todolist/MainActivity.java
+++ app/src/main/java/comp5216/sydney/edu/au/todolist/MainActivity.java
@@ -1,19 +1,48 @@
 package comp5216.sydney.edu.au.todolist;
 
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.EditText;
 import android.widget.ListView;
+import android.widget.Toast;
+
 import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
+
     // Define variables
     ListView listView;
     ArrayList<String> items;
     ArrayAdapter<String> itemsAdapter;
     EditText addItemEditText;
+
+    // Register a request to start an activity for result and register the result callback
+    ActivityResultLauncher<Intent> mLauncher = registerForActivityResult(
+            new ActivityResultContracts.StartActivityForResult(),
+            result -> {
+                if (result.getResultCode() == RESULT_OK) {
+                    // Extract name value from result extras
+                    String editedItem = result.getData().getExtras().getString("item");
+                    int position = result.getData().getIntExtra("position", -1);
+                    items.set(position, editedItem);
+                    Log.i("Updated item in list ", editedItem + ", position: " + position);
+                    // Make a standard toast that just contains text
+                    Toast.makeText(getApplicationContext(), "Updated: " + editedItem, Toast.LENGTH_SHORT).show();
+                    itemsAdapter.notifyDataSetChanged();
+                }
+            }
+    );
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -37,7 +66,9 @@ public class MainActivity extends AppCompatActivity {
 
         // Connect the listView and the adapter
         listView.setAdapter(itemsAdapter);
+        setupListViewListener();
     }
+
 
     public void onAddItemClick(View view) {
         String toAddString = addItemEditText.getText().toString();
@@ -45,5 +76,46 @@ public class MainActivity extends AppCompatActivity {
             itemsAdapter.add(toAddString); // Add text to list view adapter
             addItemEditText.setText("");
         }
+    }
+
+
+    private void setupListViewListener() {
+        listView.setOnItemLongClickListener(new AdapterView.OnItemLongClickListener() {
+            public boolean onItemLongClick(AdapterView<?> parent, View view, final int position, long rowId) {
+                Log.i("MainActivity", "Long Clicked item " + position);
+                AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
+                builder.setTitle(R.string.dialog_delete_title)
+                    .setMessage(R.string.dialog_delete_msg)
+                    .setPositiveButton(R.string.delete, new DialogInterface.OnClickListener() {
+                        public void onClick(DialogInterface dialogInterface, int i) {
+                            items.remove(position); // Remove item from the ArrayList
+                            itemsAdapter.notifyDataSetChanged(); // Notify listView adapter to update the list
+                        } })
+                    .setNegativeButton(R.string.cancel, new DialogInterface.OnClickListener() {
+                        public void onClick(DialogInterface dialogInterface, int i) {
+                            // User cancelled the dialog
+                            // Nothing happens
+                        }
+                    });
+                builder.create().show();
+                return true;
+            }
+        });
+
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                String updateItem = (String) itemsAdapter.getItem(position);
+                Log.i("MainActivity", "Clicked item " + position + ": " + updateItem);
+                Intent intent = new Intent(MainActivity.this, EditToDoItemActivity.class);
+                if (intent != null) {
+                    // put "extras" into the bundle for access in the edit activity
+                    intent.putExtra("item", updateItem);
+                    intent.putExtra("position", position);
+                    // brings up the second activity
+                    mLauncher.launch(intent);
+                    itemsAdapter.notifyDataSetChanged(); }
+            }
+        });
     }
 }
